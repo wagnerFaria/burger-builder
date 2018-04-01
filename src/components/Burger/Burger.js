4aa993137a387044c8c07d353b979b6591d13e5e
Wagner Faria do Amaral <wagneramaral@sedec.mt.gov.br>
2018-03-31 21:31:56 -0400

    Aula 113 - CSS not rendering

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = () => {
+
+    return (
+        <div className={classes.Buger}>
+            <BurgerIngredient type="bread-top" />
+            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="meat" />
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+};
+
+export default burger;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+
+
+    render() {
+
+
+        return (
+            <Aux>
+                <Burger />
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
+
+
+
+
